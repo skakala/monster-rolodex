e2f9f008ed04e67c5a57e50076b44ed7ff14aedd
skala <skakala@wp.pl>
2020-04-27 20:32:49 +0200

    Updates

---
+++ precache-manifest.ea8862c007d8570d793b5419072fad01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26124de3d30b0273766821a6cb46aa08",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3fb896f68c3dd9947e77",
+    "url": "/monster-rolodex/static/css/main.8f6151ba.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb896f68c3dd9947e77",
+    "url": "/monster-rolodex/static/js/main.49ed76f2.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
